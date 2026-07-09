35331196f35f91cd804dff8f6721e7d0226cdc00
shaikh shadab <87128116+0sha-dow0@users.noreply.github.com>
2026-07-08 22:34:23 -0700

    depcover: src/routes/products.js

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const axios = require('axios');
 const { getJson, postJson } = require('../clients/upstream');
 const { checkStock, fetchFromSource } = require('../services/inventory');
 
